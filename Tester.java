54102d2b816d1f322d1c15565ceda1bddf51293d
xkobax <chadoboy@gmail.com>
2013-07-27 15:04:14 +0300

    0.2

--- Tester.java
+++ Tester.java
@@ -4,10 +4,10 @@ package com.ciklum.study.timemap;
 public class Tester  {
 
     public static void main(String[] args) throws Exception {
-        TimeMap map = new TimeMap();
-        map.put("1", "1dsfdfsd");
-        Thread.sleep(5003);
+        TimeMap<String,String> map = new TimeMap<String,String>();
+        map.put(null, "1dsfdfsd");
+        Thread.sleep(50);
 
-        System.out.println(map.get("1"));
+        System.out.println(map.get(null));
     }
 }
